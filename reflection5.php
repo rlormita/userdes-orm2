514e00c8c05fe5e6f1f4b744f4c5ba132487e3d3
Rommel Ormita <52182399+rlormita@users.noreply.github.com>
2019-09-10 23:36:16 +0800

    reflections

---
+++ reflection5.php
@@ -0,0 +1,136 @@
+<?php header( 'Location: /reflection5.html' ) ;  ?>
+<html>
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Reflection #5</title>
+
+  <!-- Custom fonts for this theme -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+  <!-- Theme CSS -->
+  <link href="css/freelancer.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">R.L.O</a>
+      <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="co llapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.html">Home</a>
+          </li>
+         <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="activities.html">Activities</a>
+          </li>
+         <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="reflections.html">Reflections</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="certificates.html">Learnings</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
+    <header style="background-image:url(img2/original.gif)" class="masthead bg-primary text-white text-center" >
+	<div class="container d-flex align-items-center flex-column">
+	
+	<!-- Masthead Avatar Image -->
+	<h1 class="masthead-heading text-uppercase mb-0">Reflection #5</h1>
+	<br>
+	<br>
+	<img class="rounded-corner" src="img2/reflection5.jpg" align="center" width="700" height="400" alt="">
+	<br>
+	<br>
+
+	<!-- Masthead Heading -->
+	<!-- Icon Divider -->
+	<div class="divider-custom divider-light">
+		<div class="divider-custom-line"></div>
+		<div class="divider-custom-icon">
+		<i class="fas fa-star"></i>
+		</div>
+		<div class="divider-custom-line"></div>
+	</div>
+	<!-- Masthead Subheading -->
+	<p class="masthead-subheading font-weight-bold mb-0">SoCITFLIX: Create What’s Next</p>	
+	</div>
+	<section class="page-section portfolio" id="portfolio">
+    <div class="container">
+        <p align="justified">This year’s SoCIT Fest, I did experience to be an audience and an organizer. I am a part of an SoCIT organization which is the Asia Pacific College – Microsoft Community. It is new for me of being an organizer on a big event and challenging to make this event happen.  Though it is stressful, seeing that you can pull off an event was satisfying and something that I am proud of.  On the other hand, there are conflicts on the preparations and on the event implementation, there are some things that I forgot to do or things that I did not know which was needed at the event. Honestly, it is a bit sad/stressed for me when it happened but its okay. 
+		Next time, I know what to do and what will be needed because at the end of the day, the more important is that we learn.I am one of the organizers of these events: </p>
+		<p align="left">&#8226; <b>Microsoft Roadshow</b> – which is a series of seminar about different topics of Microsoft technologies.</p>
+		<p align="justified">&#8226; <b>Internet of Things</b></p>	
+		<p align="justified">&#8226; <b>Introduction to Cognitive Services</b></p>	
+		<p align="justified">&#8226; <b>Introduction to Cloud Computing with Microsoft Azure</b></p>			
+		<p align="left">&#8226; <b>Install Fest</b> - the officers will help the students to activate their Office 365 descriptions and get a free product key of Windows10 Education.</p>
+		<p align="left">&#8226; <b>SoCIT Night</b> – it is a social event where students have the chance to party with their Netflix Theme. This event will start by introducing the contestants for mini pageant,
+		there will an intermission number from PAG, outside bands will get to perform, and there will be an open floor for DJ.</p>
+		
+		<p align="justified">I also attended and join the SNATCH (Capture the Flag). It is an event of JISSA in which a professional in the field of cybersecurity, and data privacy, taught and hone our skills in cybersecurity. 
+		It is new for me to attend this kind of event, which is very fun and exciting. What we did was to find clues that are scattered on the internet and decode cryptic messages on the file given by the speaker.
+		It was test of critical thinking and teamwork to find the clues. I had fun. I hope that JISSA would do this event again next time.</p>
+		
+		<p align="justified">Next year, as an organizer of the next SoCIT FEST, I will use my learnings on this year’s SoCIT Fest in order for me to avoid mistakes next year. 
+		I hope that the next year SoCIT FEST would be more fun, organized andwill have an impact to the SoCIT Students of Asia Pacific College.</p>
+		
+		<p align="justified"><b>PHOTOS: </b></p>
+		
+
+		<img class="rounded-corner" src="img2/socitpic0/socitpic0.jpg" align="center" width="600" height="400" alt="">
+		<p align="center">With my Co-SoCIT Org Presidents</p>
+		<p></p>
+		
+	</div>
+	</section>
+  </header>
+  <footer class="footer text-center">
+    <div class="container">
+      <div class="column">
+		
+        <!-- Footer Social Icons -->
+        <div class="col-lg-4 mb-5 mb-lg-0">
+          <h4 class="text-uppercase mb-4">Around the Web</h4>
+          <a class="btn btn-outline-light btn-social mx-1" href="https://b-m.facebook.com/rmlrmt">
+            <i class="fab fa-fw fa-facebook-f"></i>
+          </a>
+          <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/rmlrmt">
+            <i class="fab fa-fw fa-twitter"></i>
+          </a>
+          <a class="btn btn-outline-light btn-social mx-1" href="https://ph.linkedin.com/in/rommel-o-29a6ba111">
+            <i class="fab fa-fw fa-linkedin-in"></i>
+          </a>
+          <a class="btn btn-outline-light btn-social mx-1" href="https://www.instagram.com/rmlrmt/">
+            <i class="fab fa-fw fa-instagram"></i>
+          </a>
+        </div>
+		
+      </div>
+    </div>
+  </footer>
+
+  <!-- Copyright Section -->
+  <section class="copyright py-4 text-center text-white">
+    <div class="container">
+	  <img src="img2/mew.gif" width="50" height="50">
+      <small>Copyright &copy; labActivity2 2019</small>
+    </div>
+  </section>
+
+</html>
